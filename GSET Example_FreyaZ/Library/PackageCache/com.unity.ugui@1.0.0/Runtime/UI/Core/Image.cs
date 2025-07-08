f36a899a34a9a441a9374e8c7c1741b101612044
yuhanz-07 <yuhaz70@uci.edu>
2025-07-08 13:54:03 -0700

    Add sth

--- GSET Example_FreyaZ/Library/PackageCache/com.unity.ugui@1.0.0/Runtime/UI/Core/Image.cs
+++ GSET Example_FreyaZ/Library/PackageCache/com.unity.ugui@1.0.0/Runtime/UI/Core/Image.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 using UnityEngine.Experimental.Rendering;
 using UnityEngine.Serialization;
 using UnityEngine.U2D;
@@ -769,14 +766,9 @@ namespace UnityEngine.UI
             {
                 if (m_Material != null)
                     return m_Material;
-
-                //Edit and Runtime should use Split Alpha Shader if EditorSettings.spritePackerMode = Sprite Atlas V2
 #if UNITY_EDITOR
-                if ((Application.isPlaying || EditorSettings.spritePackerMode == SpritePackerMode.SpriteAtlasV2) &&
-                    activeSprite && activeSprite.associatedAlphaSplitTexture != null)
-                {
+                if (Application.isPlaying && activeSprite && activeSprite.associatedAlphaSplitTexture != null)
                     return defaultETC1GraphicMaterial;
-                }
 #else
 
                 if (activeSprite && activeSprite.associatedAlphaSplitTexture != null)
@@ -1853,7 +1845,7 @@ namespace UnityEngine.UI
         {
             Rect spriteRect = activeSprite.rect;
             if (type == Type.Simple || type == Type.Filled)
-                return new Vector2(spriteRect.position.x + local.x * spriteRect.width / rect.width, spriteRect.position.y + local.y * spriteRect.height / rect.height);
+                return new Vector2(local.x * spriteRect.width / rect.width, local.y * spriteRect.height / rect.height);
 
             Vector4 border = activeSprite.border;
             Vector4 adjustedBorder = GetAdjustedBorders(border / pixelsPerUnit, rect);
@@ -1882,7 +1874,7 @@ namespace UnityEngine.UI
                 }
             }
 
-            return local + spriteRect.position;
+            return local;
         }
 
         // To track textureless images, which will be rebuild if sprite atlas manager registered a Sprite Atlas that will give this image new texture
